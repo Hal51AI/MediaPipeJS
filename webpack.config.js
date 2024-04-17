8f353d962ce60584868e76be9b985d7209511213
Sang Han <sanghan@protonmail.com>
2024-04-17 13:16:29 -0700

    add html template in webpack deployment

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 
 module.exports = {
   mode: 'production',
@@ -7,6 +9,11 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'public/index.html'
+    }),
+  ],
   module: {
     rules: [
       { test: /\.css$/, use: ['style-loader', 'css-loader'] },
